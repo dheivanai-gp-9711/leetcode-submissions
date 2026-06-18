297636ab8c95dfb526ec6d3b29171b34cbb32a40
Dheiva Gandhi <dheivanai.gp@zohocorp.com>
2026-06-18 14:52:05 +0530

    Time: 13 ms (28.53%), Space: 16.1 MB (54.57%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda i:i[0])
+        result=[]
+        result.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= result[-1][1]:
+                result[-1][0] = min(result[-1][0], intervals[i][0])
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+            else:
+                result.append(intervals[i])
+        return result 
+        
