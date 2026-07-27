2c8c01339027090d8762f9eb045bb3aa1d059632
Dheiva Gandhi <dheivanai.gp@zohocorp.com>
2026-07-27 20:41:22 +0530

    Time: 78 ms (7.25%), Space: 24.4 MB (68.16%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        map=set()
+        windowSize = k
+        for i, val in enumerate(nums):
+            if val in map:
+                return True
+            map.add(val)
+            if len(map) > k:
+                map.remove(nums[i-k])
+            
+        return False
+            
+
+        
